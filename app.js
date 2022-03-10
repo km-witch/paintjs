7e8cdce1a27000add43025a0b8b506c1905c3b08
km-witch <kmkim@witchcompany.io>
2022-03-11 00:48:54 +0900

    final

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
+const fill = document.getElementById("jsFill");
+const save = document.getElementById("jsSave");
 
 let isClicking = false;
 
-function onMouseMove(e){
+function onMouseMove(e) {
+    if (!isClicking) return;
+
     const x = e.offsetX;
     const y = e.offsetY;
 
-    if(!isClicking){
-        //context.beginPath();
-        //context.moveTo(x, y);
-    } else{
-        context.lineTo(x, y);
-        context.stroke();
-    }
+    context.lineTo(x, y);
+    context.stroke();
 }
 
 function startClicking(e) {
@@ -28,35 +27,66 @@ function endClicking(e) {
     context.closePath();
 }
 
-function changeColor(e){
+function changeColor(e) {
     console.log(e.target.style.backgroundColor);
     context.strokeStyle = e.target.style.backgroundColor;
 }
 
-function changeBrushSize(e){
+function changeBrushSize(e) {
     console.log(e.target.value);
     context.lineWidth = e.target.value;
 }
 
-if(canvas){
+function fillCanvas(e) {
+    console.log("fill");
+    context.fillStyle = context.strokeStyle;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function handleContext(e) {
+    e.preventDefault();
+}
+
+function saveImage(e) {
+    const imageURL = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = "paintJS.png";
+    link.click();
+}
+
+if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startClicking);
     canvas.addEventListener("mouseup", endClicking);
     canvas.addEventListener("mouseout", endClicking);
+    canvas.addEventListener("contextmenu", handleContext);
 
     canvas.width = 700;
     canvas.height = 700;
+
 }
 
-if(context){
+if (context) {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
     context.strokeStyle = "black";
     context.lineWidth = 5;
 }
 
-if(colors){
+if (colors) {
     Array.from(colors).forEach(x => x.addEventListener("click", changeColor));
 }
 
-if(range){
+if (range) {
     range.addEventListener("change", changeBrushSize);
+}
+
+if (fill) {
+    fill.addEventListener("click", fillCanvas);
+}
+
+if (save) {
+    save.addEventListener("click", saveImage);
 }
